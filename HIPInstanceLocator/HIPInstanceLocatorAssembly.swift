85dae50d5b60836b131d44d2c4bd11ab7c562d9f
Steve Johnson <sjohnson@hipmunk.com>
2016-07-29 14:18:56 -0700

    Make things public that should be public

--- HIPInstanceLocator/HIPInstanceLocatorAssembly.swift
+++ HIPInstanceLocator/HIPInstanceLocatorAssembly.swift
@@ -12,25 +12,25 @@ import Foundation
  Instance locator assemblies are used to encapsulate a set of locator registrations so that they may be located near
  code that uses them. Assemblies are applied to instance locators using the `assemble` method.
  */
-struct HIPInstanceLocatorAssembly {
+public struct HIPInstanceLocatorAssembly {
     private let _assemblyBlock: HIPInstanceLocator -> Void
 
     /** 
      Creates a new instance locator assembly with the given assembly block. The block is executed whenever this
      assembly is applied to an instance locator.
      */
-    init(assemblyBlock: HIPInstanceLocator -> Void) {
+    public init(assemblyBlock: HIPInstanceLocator -> Void) {
         _assemblyBlock = assemblyBlock
     }
 }
 
-extension HIPInstanceLocator {
+public extension HIPInstanceLocator {
     /**
      Applies one or more assemblies to this instance locator. Each assembly is applied in order. If the same dependency
      is registered or injected more than once, then this will throw a debug assert in the same way that would happen if
      those dependencies were registered or injected outside of an assembly.
      */
-    func assemble(assemblies: HIPInstanceLocatorAssembly...) {
+    public func assemble(assemblies: HIPInstanceLocatorAssembly...) {
         assemblies.forEach { $0._assemblyBlock(self) }
     }
 }
