8e1394eba5dfc722f0543e9396a04bbea68a0c8e
Steve Johnson <sjohnson@hipmunk.com>
2016-07-29 13:17:39 -0700

    Initial Commit

---
+++ HIPInstanceLocator/HIPInstanceLocator.h
@@ -0,0 +1,19 @@
+//
+//  HIPInstanceLocator.h
+//  HIPInstanceLocator
+//
+//  Created by Steve Johnson on 7/29/16.
+//  Copyright © 2016 Hipmunk, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HIPInstanceLocator.
+FOUNDATION_EXPORT double HIPInstanceLocatorVersionNumber;
+
+//! Project version string for HIPInstanceLocator.
+FOUNDATION_EXPORT const unsigned char HIPInstanceLocatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HIPInstanceLocator/PublicHeader.h>
+
+
